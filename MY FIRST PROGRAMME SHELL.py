d4293b4425d88cc74f0c95708bf42facd24a6575
adsigan <134690192+adsigan@users.noreply.github.com>
2023-05-26 15:27:21 +0530

    Add files via upload

---
+++ MY FIRST PROGRAMME SHELL.py
@@ -0,0 +1,16 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:34:50) [MSC v.1934 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/SSRS Com/AppData/Local/Programs/Python/Python311-32/MY FIRST PROGRAMME.py
+Hello, World!
+What is your name?Ravventhiran Adsigan
+Hello Ravventhiran Adsigan
+>>> age=14
+>>> print(age)
+14
+>>> weight=47
+>>> print(weight)
+47
+>>> height=153
+>>> print(height)
+153
